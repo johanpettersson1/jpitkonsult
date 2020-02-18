98afb336ca0f2df33e77d0dd0c0ccb3d9b257a26
Johan Pettersson <johanp7@kth.se>
2020-02-18 12:17:04 +0100

    init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+// Add scrollspy to <body>
+
+var offset = 127;
+
+$('.navbar li a').click(function (event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+});
